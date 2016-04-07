0f14bdb73ccca5a3a67d40689cfe28c2d24dd2bd
Roger1 <kylodonu@st.ug.edu.gh>
2016-04-07 23:28:30 +0000

    DisplayingAFile class created

---
+++ src/assignment4_10518900/DisplayingAFile.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment4_10518900;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author kenny
+ */
+public class DisplayingAFile {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        String name="";
+        System.out.print("Open with file: ");
+        name=input.nextLine();
+        try{
+            File file=new File("/home/kenny/NetBeansProjects/Assignment4_10518900"
+                    + "/src/assignment4_10518900/files/"+name);
+            Scanner sc=new Scanner(file);
+            while(sc.hasNext()){
+                System.out.println(sc.nextLine());
+            }
+        }catch(FileNotFoundException e){
+            e.printStackTrace();
+        }
+    }
+    
+}
