5c9a7b2e4a1a1006b15866892ae334ae3319ac2f
Roger1 <kylodonu@st.ug.edu.gh>
2016-04-08 07:43:52 +0000

    BasicArrays0 class created

---
+++ src/Arrays/BasicArrays0.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Arrays;
+
+/**
+ *
+ * @author kenny
+ */
+public class BasicArrays0 {
+    public static void main(String[] args) {
+        int [] nums={-113,-113,-113,-113,-113,-113,-113,-113,-113,-113};
+        
+        System.out.println("Slot "+0+" contains a "+nums[0]);
+        System.out.println("Slot "+1+" contains a "+nums[1]);
+        System.out.println("Slot "+2+" contains a "+nums[2]);
+        System.out.println("Slot "+3+" contains a "+nums[3]);
+        System.out.println("Slot "+4+" contains a "+nums[4]);
+        System.out.println("Slot "+5+" contains a "+nums[5]);
+        System.out.println("Slot "+6+" contains a "+nums[6]);
+        System.out.println("Slot "+7+" contains a "+nums[7]);
+        System.out.println("Slot "+8+" contains a "+nums[8]);
+        System.out.println("Slot "+9+" contains a "+nums[9]);
+        
+        
+    }
+    
+}
