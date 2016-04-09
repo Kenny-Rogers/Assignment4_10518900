f5b28fa9a348fc1a5f36fbba559e759df06f81e4
Roger1 <kylodonu@st.ug.edu.gh>
2016-04-09 04:54:51 +0000

    BasicArrayList0 class created.

---
+++ src/ArrayList/BasicArrayList0.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ArrayList;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author kenny
+ */
+public class BasicArrayList0 {
+    public static void main(String[] args) {
+        ArrayList<Integer> nums=new ArrayList<>();
+        nums.add(-113);
+        nums.add(-113);
+        nums.add(-113);
+        nums.add(-113);
+        nums.add(-113);
+        nums.add(-113);
+        nums.add(-113);
+        nums.add(-113);
+        nums.add(-113);
+        nums.add(-113);
+        
+        System.out.println("Slot "+0+" contains a "+nums.get(0));
+        System.out.println("Slot "+1+" contains a "+nums.get(1));
+        System.out.println("Slot "+2+" contains a "+nums.get(2));
+        System.out.println("Slot "+3+" contains a "+nums.get(3));
+        System.out.println("Slot "+4+" contains a "+nums.get(4));
+        System.out.println("Slot "+5+" contains a "+nums.get(5));
+        System.out.println("Slot "+6+" contains a "+nums.get(6));
+        System.out.println("Slot "+7+" contains a "+nums.get(7));
+        System.out.println("Slot "+8+" contains a "+nums.get(8));
+        System.out.println("Slot "+9+" contains a "+nums.get(9));
+        
+    }
+    
+}
