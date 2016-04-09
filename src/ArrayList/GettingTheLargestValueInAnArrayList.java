23c27e9309c904f99e4525be6b4c832a17b1f4d5
Roger1 <kylodonu@st.ug.edu.gh>
2016-04-09 20:20:44 +0000

    GettingTheLargestValueInAnArrayList class created.

---
+++ src/ArrayList/GettingTheLargestValueInAnArrayList.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ArrayList;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ *
+ * @author kenny
+ */
+public class GettingTheLargestValueInAnArrayList {
+    public static void main(String[] args) {
+        Random r=new Random();
+        ArrayList<Integer> arr=new ArrayList<>();
+        int larg=0;
+        
+        for (int i = 0; i < 10; i++) 
+            arr.add(1+r.nextInt(99));
+        
+        System.out.println("ArrayList: "+arr);
+        
+        larg=arr.get(0);
+        for(int i=0; i<10; i++){
+            if(larg<arr.get(i))
+                larg=arr.get(i);
+        }
+        
+        System.out.println("\nThe largest value is "+larg);
+    }
+    
+}
